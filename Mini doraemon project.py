3b7214aa5c1b1e44bc83dfb171e21f36ccb3e396
Akshita Sharma <70146188+akshita-sharma8609@users.noreply.github.com>
2021-06-20 18:46:20 +0530

    Mini_doraemon_desktop_voice_assistant modified

--- Mini doraemon project.py
+++ Mini doraemon project.py
@@ -19,7 +19,7 @@ def greetuser():
     hour=datetime.datetime.now().hour
     if(hour>0 and hour <12):
         speak("Good Morning")
-    elif(hour>=12 and hour<=18):
+    elif(hour>=12 and hour<18):
         speak("Good afternoon")
     else:
         speak("Good evening")
